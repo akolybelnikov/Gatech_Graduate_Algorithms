345149654ad6bc3633b2df4555fe49467d173d3d
akolybelnikov <a.kolybelnikov@gmail.com>
2019-09-04 23:51:12 +0200

    Testing knapsack without repetition

--- src/bin/knapsack.rs
+++ src/bin/knapsack.rs
@@ -3,21 +3,42 @@ use std::cmp::max;
 fn knapsack(wt: &Vec<i32>, val: &Vec<i32>, b: i32) -> i32 {
     let mut grid = vec![vec![0; (b + 1) as usize]; (wt.len() + 1) as usize];
 
-    for i in 1..(wt.len() + 1) {
+    for i in 1..(wt.len() + 1) as usize {
         for j in 1..(b + 1) {
             if wt[i - 1] <= j {
-                grid[i as usize][j as usize] = max(
-                    grid[(i - 1) as usize][j as usize],
-                    grid[(i - 1) as usize][(j - wt[i - 1]) as usize] + val[i - 1],
+                grid[i][j as usize] = max(
+                    grid[i - 1][j as usize],
+                    grid[i - 1][(j - wt[i - 1]) as usize] + val[i - 1],
                 );
             } else {
-                grid[i as usize][j as usize] = grid[(i - 1) as usize][j as usize];
+                grid[i][j as usize] = grid[i - 1][j as usize];
             }
         }
-        println!("{:?}", grid[i]);
     }
     println!("{}", grid[wt.len() as usize][b as usize]);
     grid[wt.len() as usize][b as usize]
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_knapsack_1() {
+        assert_eq!(knapsack(&vec![6, 3, 4, 2], &vec![30, 14, 16, 9], 10), 46);
+    }
+
+    #[test]
+    fn test_knapsack_2() {
+        assert_eq!(
+            knapsack(
+                &vec![4, 2, 3, 5, 5, 6, 9, 7, 8, 10],
+                &vec![22, 20, 15, 30, 24, 54, 21, 32, 18, 25],
+                30
+            ),
+            182
+        );
+    }
 }
 
 fn main() {
